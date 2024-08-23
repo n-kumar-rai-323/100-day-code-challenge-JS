8eee57a1eecaa4bcd240410fc83eb5ce86048129
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-08-23 20:04:08 +0545

    Day Three

--- Day-three.js
+++ Day-three.js
@@ -6,3 +6,23 @@ const reverseString = (text)=>{
 
 let Result = reverseString("Nishan")
 console.log(Result)
+
+
+// / Reference By MDN 
+// The split() method of String values takes a pattern and
+//  divides this string into an ordered list of substrings by searching for 
+// the pattern, puts these substrings into an array, and returns the array.
+
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+const words = str.split(' ');
+console.log(words[3]);
+// Expected output: "fox"
+
+const chars = str.split('');
+console.log(chars[8]);
+// Expected output: "k"
+
+const strCopy = str.split();
+console.log(strCopy);
+// Expected output: Array ["The quick brown fox jumps over the lazy dog."]
